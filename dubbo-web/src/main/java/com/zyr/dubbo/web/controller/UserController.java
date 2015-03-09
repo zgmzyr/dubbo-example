0f894a4046c5bc83114a56f5982cd2f851a5ae48
zgm <zgm@zhouguangminghost>
2015-03-09 17:59:51 +0800

    init

--- dubbo-web/src/main/java/com/zyr/dubbo/web/controller/UserController.java
+++ dubbo-web/src/main/java/com/zyr/dubbo/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.zyr.dubbo.web.controller;
 
-import org.springframework.stereotype.Controller;
+import javax.annotation.Resource;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zyr.dubbo.common.domain.User;
 import com.zyr.dubbo.common.service.UserReadService;
 
 @Controller
@@ -9,5 +14,13 @@ public class UserController {
 	
 	@Resource(name = "userReadServiceClient")
 	private UserReadService userReadService;
+	
+	@RequestMapping("user/queryUserById")
+	@ResponseBody
+	public String queryUserById(int id) {
+		User user = userReadService.queryUserById(id);
+		
+		return user.getName();
+	}
 
 }
